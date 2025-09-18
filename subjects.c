848a98b205c2d29a42bf36ef656a9489ae2714ae
peter127-prog <peterthagana90@gmail.com>
2025-09-18 18:16:48 +0300

    Add files via upload

---
+++ subjects.c
@@ -0,0 +1,21 @@
+//simple c program
+/*
+name:peter thagana
+reg:CT100/G/26164/25
+desc:my semester units
+*/
+#include<stdio.h>
+
+int main ( )
+{
+	printf("mathematic for science \n");
+	printf("computer systems and organisation \n");
+	printf("fundamentals of ict \n");
+	printf("essentials of accounting \n");
+	printf("analogue electronics \n");
+	printf("intriduction to programming \n");
+	printf("communication skills \n");
+	printf("introduction to computer application \n");
+	return 0;
+}
+	
