f9a7cb939848ab8beef52ad87e557d1ab98e3f84
peter127-prog <peterthagana90@gmail.com>
2025-09-25 18:26:24 +0300

    Add files via upload

---
+++ compoundinterest.c
@@ -0,0 +1,25 @@
+/*
+name:Peter
+reg:CT100/G/26164/25
+desc:calculation of compound interest
+*/
+#include<stdio.h>
+
+int main()
+{
+float principle,rate,time,amount,n;
+
+printf("enter principle");
+scanf("%f", &principle);
+
+printf("enter time");
+scanf("%f", &time);
+
+printf("enter rate");
+scanf("%f", &rate);
+
+//compound interest
+amount=principle*(1+rate/n),(n+time);
+
+return 0;
+}
