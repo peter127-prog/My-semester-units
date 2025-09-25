0626d924ab5525e18b66b200df15a28fd52036e0
peter127-prog <peterthagana90@gmail.com>
2025-09-25 08:15:12 +0300

    Updatevolumeandsurfacearea.c

--- volume and surface area.c
+++ volume and surface area.c
@@ -9,19 +9,10 @@ int main()
 {
 float radius,height,volume,π,area;
 printf("enter radius :");
-scanf("%f",  radius);
+scanf("%f",  &radius);
 
 printf("enter height : ");
 scanf("%f",  &height);
-
-printf("enter volume : ");
-scanf("%f", &volume);
-
-printf("enter π : ");
-scanf("%f", &π);
-
-printf("enter area : ");
-scanf("%f", &area);
 
 volume=3.142*radius*radius*height;
 area=2*3.142*radius*radius+2*3.142*radius*height;
