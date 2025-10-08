c949c536c8c2d35d98fa940b669e77ac01904d13
peter127-prog <peterthagana90@gmail.com>
2025-10-08 18:56:34 +0300

    Add files via upload

---
+++ for_loop_numbers_from100.c
@@ -0,0 +1,15 @@
+/*
+name:peter
+reg:CT100/G/26164/25
+desc:a for loop for descending numbers from 100 to 50
+*/
+#include<stdio.h>
+
+int main(){
+	int i;
+	for(i = 100; i >= 50; i--) {
+		printf("%d \n",  i);
+	}
+	
+return 0;
+}
